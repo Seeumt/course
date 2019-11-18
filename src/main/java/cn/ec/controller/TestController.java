4ff9ab167810f5fb65ffe593ee796baf7d1a7a03
Seeumt <Seeumt@cumt.edu.cn>
2019-11-18 15:51:22 +0800

    SpringMVC!!!

--- src/main/java/cn/ec/controller/TestController.java
+++ src/main/java/cn/ec/controller/TestController.java
@@ -1,13 +1,17 @@
 package cn.ec.controller;
 
 import cn.ec.pojo.Builder;
+import cn.ec.pojo.Person;
 import cn.ec.pojo.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -79,7 +83,6 @@ public class TestController {
 
     @RequestMapping("/c4/{id}/{name}")
     public ModelAndView c4(ModelAndView mv,@PathVariable Integer id, @PathVariable String name){
-
         mv.addObject("id",id);
         mv.addObject("name",name);
         mv.setViewName("ok");
@@ -94,6 +97,76 @@ public class TestController {
         builder.setName(name);
         return builder;
     }
+
+    @GetMapping("/c6/{id}/{name}")
+    @ResponseBody
+    public Builder c6(@PathVariable Integer id, @PathVariable String name,
+                      @MatrixVariable(name = "num",pathVar = "id") Integer[] num,
+                      @MatrixVariable(name = "date",pathVar = "id") Integer date){
+        Builder builder = new Builder();
+        builder.setId(id);
+        builder.setName(name);
+        System.out.println(num);
+        for (Integer item : num) {
+            System.out.println(item);
+        }
+        return builder;
+    }
+
+    @GetMapping("/c7/{id}/{name}")
+    @ResponseBody
+    public Builder c7(@PathVariable Integer id, @PathVariable String name,
+                      @MatrixVariable(name = "num",pathVar = "name") Integer num,
+                      @MatrixVariable(name = "date",pathVar = "name") Integer date){
+        Builder builder = new Builder();
+        builder.setId(id);
+        builder.setName(name);
+        System.out.println(num);
+        System.out.println(date);
+        return builder;
+    }
+
+    @PostMapping("/c8")
+    public String c8(@RequestPart("file") MultipartFile file) {
+            String filePath = "C:\\" + file.getOriginalFilename();
+        try {
+            file.transferTo(new File(filePath));
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        return "ok";
+    }
+
+    @RequestMapping("/c9")
+    public String c9(@RequestPart("file") MultipartFile[] files) {
+        for(MultipartFile file : files){
+            String filePath = "C:\\" + file.getOriginalFilename();
+            try {
+                file.transferTo(new File(filePath));
+            }catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+        }
+        return "ok";
+    }
+
+    @RequestMapping("/c10")
+    public String c10(@CookieValue("JSESSIONID") String sessionId) {
+        System.out.println(sessionId);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/c11", method = RequestMethod.POST, consumes="application/json")
+    public String c11(HttpServletRequest req ,@RequestBody(required = false) Builder builder) {
+        System.out.println(builder);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/c12", method = RequestMethod.POST, consumes="application/xml")
+    public void c12(HttpServletRequest req ,@RequestBody(required = false) Person person) {
+        System.out.println(person);
+    }
+
 
 
 }
---
+++ src/main/java/cn/ec/pojo/Person.java
@@ -0,0 +1,14 @@
+package cn.ec.pojo;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+@JacksonXmlRootElement(localName = "school")
+public class Person {
+    @JacksonXmlProperty(localName = "id")
+    private Integer id;
+    @JacksonXmlProperty(localName = "name")
+    private String name;
+    @JacksonXmlProperty(localName = "shortname")
+    private String shortName;
+    //……
+}
